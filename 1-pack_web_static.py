3c5004199a2fc4f985fbfcb05a2db1b5265f96e7
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-04-20 09:18:07 +0000

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script to generates a .tgz archive from the contents of the web_static folder."""
+"""Generate a .tgz archive from web_static folder."""
 from datetime import datetime
 from fabric.api import local
 
