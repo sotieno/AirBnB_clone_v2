e2b402da577824e5c568191138766634f6cf544e
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-05-11 11:34:13 +0000

    Add 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,33 +8,33 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-    """ Print Web """
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ Print Web """
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """ Print a char C followed by the value of the text variable """
+    """display “C ” followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
 def python_is_cool(text='is cool'):
-    """ Print Python, followed by the value of the text variable,
+    """display “Python ”, followed by the value of the text variable,
     with default value of text: is cool """
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>')
 def number(n):
-    """ number route """
+    """display “n is a number” only if n is an integer"""
     return '{:d} is a number'.format(n)
 
 
