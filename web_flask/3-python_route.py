f853e2b4c8e0bc18aab731cf9a45338fb00e748e
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-05-11 11:30:57 +0000

    Add 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,26 +8,26 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-    """ Print Web """
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ Print Web """
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """ Print a char C followed by the value of the text variable """
+    """display “C ” followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
 def python_is_cool(text='is cool'):
-    """ Print Python, followed by the value of the text variable,
+    """display “Python ”, followed by the value of the text variable,
     with default value of text: is cool"""
     return 'Python {}'.format(text.replace('_', ' '))
 
