e8610ec08e21eb2750e3e39ec2fb92b914b740b8
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-05-12 11:00:03 +0000

    Add cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
 """
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def closedb(exc):
-    """ to close a database session"""
-    storage.close()
-
-
-@app.route('/cities_by_states')
-def states_list():
-    """ /states_list route """
-    states = storage.all(State).values()
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
 if __name__ == '__main__':
-    storage.reload()
-    app.run("0.0.0.0", 5000)
+    app.run(host='0.0.0.0', port='5000')
