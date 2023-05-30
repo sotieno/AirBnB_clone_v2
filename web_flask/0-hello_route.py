444c9cf2ad980fe9f40b61b80e5129b0f0b5f8ce
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-05-30 09:44:48 +0000

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env bash
 """ Script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
-@app.route('/')
-def hello_hbnb():
+@app.route('/airbnb-onepage/')
+def airbnb_onepage():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
